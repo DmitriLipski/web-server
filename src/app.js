665124877bd1030ee1c48f57b19fc8a35b1f0be3
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-04-01 21:45:32 +0300

    Add handlebars

--- src/app.js
+++ src/app.js
@@ -4,12 +4,33 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Dreamer'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Jan Dreamer'
+    })
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.'
+    })
+});
 
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
-        location: 'Philadelphia'
+        location: 'Minsk'
     })
 });
 
